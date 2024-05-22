14468d094bf63ddca7ea2571ea29a7d90af6a3d5
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-05-22 20:35:22 +0330

    commietd

---
+++ Employee.cpp
@@ -0,0 +1,96 @@
+#include"Employee.h"
+#include<iostream>
+using namespace std;
+Employee::Employee(string n,string i,int a,int b,int c,int d): name(n),id(i),hourWork(a),salaryPerHour(b),workToDo(c),workDone(d){
+if(validate(id)==false)
+cout<<"invalid id"<<endl;
+}
+
+Employee::Employee (const Employee& other){
+     hourWork=other. hourWork;
+     salaryPerHour=other.salaryPerHour;
+     workToDo=other.workToDo;
+     workDone=other.workDone;
+     name=other.name;
+     id=other.id;
+}
+ostream& operator<<(ostream& str,const Employee& employee ){
+ str << employee.name << employee.id <<  employee.address
+            << employee.hourWork << employee.salaryPerHour
+             <<employee.workToDo << employee.workDone<<endl;
+return str;
+}
+istream& operator>>(istream& str, Employee& employee){
+str >> employee.name >> employee.id >>employee.address
+            >>employee.hourWork >> employee.salaryPerHour
+             >>employee.workToDo >>employee.workDone;
+return str;
+}
+Employee Employee::operator=(const Employee& other){
+hourWork=other.hourWork;
+salaryPerHour=other.salaryPerHour;
+workToDo=other.workToDo;
+workDone=other.workDone;
+name=other.name;
+id=other.id;
+return *this;
+}
+    string Employee::getname()const{return name;}
+    void Employee::setname(string a) {name=a;}
+
+    string Employee::getid()const{return id;}
+    void Employee::setid(string a){id=a;}
+
+    int Employee::hourwork()const{return hourWork;}
+    void Employee::hourwork(int d){ hourWork=d;}
+
+    int Employee::getsalaryperhour()const{return salaryPerHour;}
+    void Employee::salaryperhour(int d){salaryPerHour=d;}
+
+    int Employee::getworktodo()const{ return workToDo; }
+    void Employee::setworktodo(int d){ workToDo=d; }
+
+    int Employee::getworkdone()const{return workDone; }
+    void Employee::setworkdone(int d){ workDone=d;}
+
+  int Employee::calculateSalary() {
+        int baseSalary = hourWork * salaryPerHour;
+        int workNotDone = workToDo - workDone;
+        double deduction = (double(workNotDone) / workToDo) * baseSalary;
+        return baseSalary - deduction;
+    }
+
+    double Employee::efficiency() {
+        return double(workDone) / (hourWork == 0 ? 1 : hourWork);
+    }
+     bool Employee::validate(string& id)  {
+        int length = id.length();
+
+        // Check length
+        if (length < 8 || length > 10) return false;
+
+        // Check the first two characters (year of entry)
+        int year = std::stoi(id.substr(0, 2));
+        if (year < 84 || year > 99) return false;
+
+        // Check the next 1-3 non-digit characters
+        int nonDigitCount = 0;
+        for (int i = 2; i < length && !std::isdigit(id[i]); ++i) {
+            ++nonDigitCount;
+        }
+        if (nonDigitCount < 1 || nonDigitCount > 3) return false;
+
+        // Check the last five characters (digits less than 4 or greater than 6)
+        for (int i = length - 5; i < length; ++i) {
+            if (std::isdigit(id[i])) {
+                int digit = id[i] - '0';
+                if (digit >= 4 && digit <= 6) return false;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    
