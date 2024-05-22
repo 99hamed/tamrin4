25f029df28e91e0a0dc445582abe864209007efb
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-05-22 20:40:46 +0330

    commited

--- Person.cpp
+++ Person.cpp
@@ -1,12 +1,72 @@
-#include "Person.h"
+#include"Person.h"
+#include"Address.h"
 #include<iostream>
+#include<cstring>
 using namespace std;
  
- Person:: Person(string n,string i,Address a): name(n),id(i),address(a){
-        validate(id);
+ Person::Person(string a,string b): name(a),id(b){
+    if(validate(id)==false)
+        cout<<"id is invalid"<<endl;
+ }
+Person::Person(const Person& a){
+    name=a.name;
+    id=a.id;
+    
+}
+
+ostream& operator<<(ostream& str,const Person& d ){
+str<<d.name<<d.id;
+return str;
+}
+istream& operator>>(istream& str, Person& d){
+str>>d.name>>d.id;
+return str;
+}
+Person Person::operator=(const Person& d){
+name=d.name;
+id=d.id;
+return *this;
+}
+    string Person::getname()const{
+    return name;
+}
+    void Person::setname(string n){
+        name=n;
     }
- Person::Person(const Person& a){
-        name=a.name;
-        id=a.id;
-        address=a.address;
+    string Person::getid()const{
+        return id;
+    }
+    void Person::setid(string i){
+        id=i;
+    }
+     bool Person::validate(string& id){
+        int length = id.length();
+
+       
+        if (length < 8 || length > 10) return false;  // Check length
+
+        
+        int year = std::stoi(id.substr(0, 2)); // Check the first two characters (year of entry)
+        if (year < 84 || year > 99) return false;
+
+        
+        int nonDigitCount = 0;  // Check the next 1-3 non-digit characters
+        for (int i = 2; i < length && !std::isdigit(id[i]); ++i) {
+            ++nonDigitCount;
         }
+        if (nonDigitCount < 1 || nonDigitCount > 3) return false;
+
+       
+        for (int i = length - 5; i < length; ++i) {  // Check the last five characters (digits less than 4 or greater than 6)
+            if (std::isdigit(id[i])) {
+                int digit = id[i] - '0';
+                if (digit >= 4 && digit <= 6) return false;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+   
